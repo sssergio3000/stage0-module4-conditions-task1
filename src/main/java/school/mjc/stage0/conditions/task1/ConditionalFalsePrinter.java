4a99f42f954c78612e8519304bafd9e27198d8f2
Sergey Khaykov <ssserg3000@gmail.com>
2022-11-25 12:52:59 +0200

    initial commit

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (false){
+            System.out.println();
+        }
+        else{
+            System.out.println("false");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
+        if(numberToBeDetermined>0){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+
     }
+
+    public static void main(String[] args) {
+        PositiveNumberDeterminer pnd = new PositiveNumberDeterminer();
+        pnd.isPositive(5);
+    }
+
 }
